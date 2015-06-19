73a26bef6bdac5d9f83111039e310f3d9b4e196f
Vincent Mahnke <V.Mahnke@gmx.com>
2015-06-19 14:00:27 +0200

    + Intended crash by using a not-locked std::vector - Sample multi-threadded output

--- multithreadding/multithreadding/multithreadding.cpp
+++ multithreadding/multithreadding/multithreadding.cpp
@@ -5,13 +5,23 @@
 
 #include <vector>
 
+struct ParamDef {
+	int* ThreadCount;
+	std::vector<int>* IntVector;
+};
+
 DWORD WINAPI NullThread(LPVOID ptrThreadIsDone)
 {
-	int* threadCount = (int*)ptrThreadIsDone;
+	// int* threadCount = (int*)ptrThreadIsDone;
+	ParamDef* ptrParameter = (ParamDef*)ptrThreadIsDone;
 
-	printf("Processor %i (ID: 0x%0x): Threads running atm: %i\r\n", GetCurrentProcessorNumber(), GetCurrentThreadId(), *threadCount);
+	printf("Processor %i (ID: 0x%0x): Threads running atm: %i\r\n", GetCurrentProcessorNumber(), GetCurrentThreadId(), *(ptrParameter->ThreadCount));
 
-	(*threadCount)--;
+	for (int i = 0; i < 100; i++) {
+		(*(*ptrParameter).IntVector).push_back(i);
+	}
+
+	(*(*ptrParameter).ThreadCount)--;
 	return 0;
 }
 
@@ -25,62 +35,69 @@ int _tmain(int argc, _TCHAR* argv[])
 	int ThreadCount = 0;
 	int ThreadID = 0;
 
-	HANDLE h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &ThreadCount, 0, (LPDWORD)&ThreadID);
+	std::vector<int> IntVector;
+
+	ParamDef parameter;
+	parameter.ThreadCount = &ThreadCount;
+	parameter.IntVector = &IntVector;
+
+	HANDLE h;
+	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &parameter, 0, (LPDWORD)&ThreadID);
 	if (h == INVALID_HANDLE_VALUE) {
 		return 1;
 	}
 	ThreadCount++;
-	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &ThreadCount, 0, (LPDWORD)&ThreadID);
+	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &parameter, 0, (LPDWORD)&ThreadID);
 	if (h == INVALID_HANDLE_VALUE) {
 		return 1;
 	}
 	ThreadCount++;
-	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &ThreadCount, 0, (LPDWORD)&ThreadID);
+	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &parameter, 0, (LPDWORD)&ThreadID);
 	if (h == INVALID_HANDLE_VALUE) {
 		return 1;
 	}
 	ThreadCount++;
-	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &ThreadCount, 0, (LPDWORD)&ThreadID);
+	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &parameter, 0, (LPDWORD)&ThreadID);
 	if (h == INVALID_HANDLE_VALUE) {
 		return 1;
 	}
 	ThreadCount++;
-	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &ThreadCount, 0, (LPDWORD)&ThreadID);
+	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &parameter, 0, (LPDWORD)&ThreadID);
 	if (h == INVALID_HANDLE_VALUE) {
 		return 1;
 	}
 	ThreadCount++;
-	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &ThreadCount, 0, (LPDWORD)&ThreadID);
+	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &parameter, 0, (LPDWORD)&ThreadID);
 	if (h == INVALID_HANDLE_VALUE) {
 		return 1;
 	}
 	ThreadCount++;
-	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &ThreadCount, 0, (LPDWORD)&ThreadID);
+	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &parameter, 0, (LPDWORD)&ThreadID);
 	if (h == INVALID_HANDLE_VALUE) {
 		return 1;
 	}
 	ThreadCount++;
-	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &ThreadCount, 0, (LPDWORD)&ThreadID);
+	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &parameter, 0, (LPDWORD)&ThreadID);
 	if (h == INVALID_HANDLE_VALUE) {
 		return 1;
 	}
 	ThreadCount++;
-	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &ThreadCount, 0, (LPDWORD)&ThreadID);
+	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &parameter, 0, (LPDWORD)&ThreadID);
 	if (h == INVALID_HANDLE_VALUE) {
 		return 1;
 	}
 	ThreadCount++;
-	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &ThreadCount, 0, (LPDWORD)&ThreadID);
+	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &parameter, 0, (LPDWORD)&ThreadID);
 	if (h == INVALID_HANDLE_VALUE) {
 		return 1;
 	}
 	ThreadCount++;
-	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &ThreadCount, 0, (LPDWORD)&ThreadID);
+	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &parameter, 0, (LPDWORD)&ThreadID);
 	if (h == INVALID_HANDLE_VALUE) {
 		return 1;
 	}
 	ThreadCount++;
-	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &ThreadCount, 0, (LPDWORD)&ThreadID);
+	h = CreateThread((LPSECURITY_ATTRIBUTES)&SecurityAttributes, (SIZE_T)&StackSize, NullThread, &parameter, 0, (LPDWORD)&ThreadID);
 	if (h == INVALID_HANDLE_VALUE) {
 		return 1;
 	}
