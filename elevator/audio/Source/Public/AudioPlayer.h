164d1d6906c9ba13a8f93b3aff685de061471c7b
Vincent Mahnke <accounts@vimaster.de>
2015-06-27 17:50:37 +0200

    ~ Seperated AudioPlayer from main-file + Added #pragma once to a few files

---
+++ elevator/audio/Source/Public/AudioPlayer.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "../Public/stdafx.h"
+
+#include <vector>
+
+#include "FileHandle.h"
+#include "WaveFormat.h"
+
+#pragma comment(lib, "winmm.lib")
+
+class AudioPlayer {
+public:
+	static void CALLBACK AudioCallback(HWAVEOUT hwo, UINT uMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2);
+private:
+	std::string Filename;
+	FILE* InternalFileHandle;
+	// Denotes the fseek-usable begin of the data-chunk of the file
+	int DataBegin = 0;
+
+	WAVEFORMATEX WaveFormat;
+	WaveFormat::Header Header;
+
+	std::vector<WaveFormat::Chunk> ChunkList;
+
+	HWAVEOUT WaveOutHandle;
+	WAVEHDR WaveHeader;
+	WAVEHDR WaveHeader2;
+
+	// Inner helper functions
+	void PrepareFile();
+	bool OpenDevice();
+	bool PrepareHeader(WAVEHDR* waveHeader);
+	bool ExecuteSound(WAVEHDR* waveHeader);
+public:
+	bool Play();
+
+	AudioPlayer(const char* filename);
+
+	~AudioPlayer();
+};
